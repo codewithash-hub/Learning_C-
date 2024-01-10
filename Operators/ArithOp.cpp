7eace69466eb4b2489eb2587d955496b38cd2fd7
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2024-01-10 11:00:19 +0200

    Arithmetic operation

---
+++ Operators/ArithOp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 20;
+    int y = 30;
+
+    cout << x + y <<endl;
+    cout << x - y <<endl;
+    cout << x / y <<endl;
+    cout << x * y <<endl;
+}
